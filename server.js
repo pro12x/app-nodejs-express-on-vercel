3462d8eaa5537793b99b09384ab0869dd4f2d930
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-09-07 17:39:10 +0000

    Deploy Test No 6

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use("/assets", express.static(path.resolve(__dirname, "frontend", "assets")))
+app.use("/assets", express.static(path.resolve(__dirname, "frontend", "assets")));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "index.html"))
-})
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
