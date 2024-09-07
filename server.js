2cfae4626629f96b1a5abc3ecf50703de0f4e44e
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-09-07 17:47:31 +0000

    Deploy Test No 8

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 app.use("/assets", express.static(path.resolve(__dirname, "frontend", "assets")));
 
